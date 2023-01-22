e9efab8fcd7d17751869f5649200e13d793dc1bc
fetibakiji <Fetibakiji202@gmail.com>
2023-01-22 01:54:36 +0100

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+// const bar = document.querySelector('#navbar');
+// const nav = document.querySelector('#bar');
+
+
+//      bar.addEventListener('click', () =>{
+
+//      nav.classList.toggle('open');
+
+//     });
